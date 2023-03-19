9d51324179d1312933880c377136e7cbb1ef2769
BahadirErsozz <b.ersoz2002@gmail.com>
2023-03-19 22:51:01 +0300

    fixed right shopping cart bar

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -5,7 +5,7 @@ const ShoppingCart = ({shoppingCart}) => {
 
     return (
         <>
-       <div style={{position: "fixed", display: "flex", flexDirection: "column" , width: "20%", height: "100%", borderStyle: "solid", borderWidth: "1px", borderColor: "black", right: "0px", zIndex: "200"}}>
+       <div style={{position: "fixed", display: "flex", flexDirection: "column" , width: "20%", height: "100%", borderStyle: "", borderWidth: "1px", borderColor: "black", right: "0px", zIndex: "200", top: "64px", backgroundColor: "white", boxShadow: "0 0.3rem 2rem rgba(0,0,0,.1)"}}>
         <div style={{width: "auto", margin: "0px 10px"}}>
         <div style={{width: "100%", height: "auto", textAlign: "center"}}>
             <div>Sepetiniz</div>
