0a0c556abae8a53a7efa55a26cd4e0740cee68bf
BahadirErsozz <b.ersoz2002@gmail.com>
2023-06-06 00:34:01 +0300

    Timer takes deadline from the database

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -19,7 +19,7 @@ const Product = ({product, addItemToCart}) => {
     return (
         <>
        <div style={{width: "calc(calc(100%/4)", maxWidth: "calc(calc(100%/4)", minWidth: "calc(calc(100%/4)", margin: "10px 5px", height: "auto", borderStyle: "solid", borderWidth: "5px", borderColor: "#dcdcdc", alignSelf: "flex-start", textAlign: "center", backgroundColor: "rgba(51,51,51,.04)", zIndex: "5", flex: "1", aspectRatio: "1 / 1"}}>
-            <Timer></Timer>
+            <Timer deadline={product.deadline}></Timer>
             <div style={{width: "100%", height: "146px", backgroundColor: "rgba(51,51,51,.04)"}}>
                 <img src="https://images.deliveryhero.io/image/fd-tr/mahalleproductimages/00000002150561.jpg?height=140&dpi=2" style={{width: "100%", height: "100%", background: "transparent" }}></img>
             </div>
--- src/components/Products/Timer.jsx
+++ src/components/Products/Timer.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const Timer = () => {
+const Timer = ({deadline}) => {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-
-  const deadline = "April, 30, 2024";
 
   const getTime = () => {
     const time = Date.parse(deadline) - Date.now();
