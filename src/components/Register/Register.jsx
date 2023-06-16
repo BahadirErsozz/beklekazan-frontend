13cc4e61b831e2cab17b04e83086538211d94548
BahadirErsozz <b.ersoz2002@gmail.com>
2023-06-16 15:19:07 +0300

    Added register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,19 @@
-const Register = ({handleQuitRegister, handleClickLogin}) => {
+import { useState } from "react";
+
+const Register = ({handleQuitRegister, handleClickLogin, register}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
+    };
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value);
+    };
+    const handleRegister = () => {
+        register(email, password)
+    }
     return (
         <>
 
@@ -6,18 +21,18 @@ const Register = ({handleQuitRegister, handleClickLogin}) => {
         <div onClick={handleQuitRegister} style={{marginRight: "10px", marginTop: "10px", marginLeft: "auto", borderStyle: "", borderRadius: "50%", height: "10%", aspectRatio: "1 / 1", backgroundSize: "100%", backgroundImage: "url(https://img.freepik.com/free-icon/x-symbol_318-1407.jpg)"}}></div>
             <div style={{  padding: "2em", paddingBottom: "0px"}}>
             E-Mail
-            <input style={{ marginBottom: "32px",width: "calc(100% - 70px)",padding: "16px 20px 16px 50px", left: "15%", alignItems: "center", alignContent: "center", display: "flex", border: "1px solid #d2d8d8"}}>
+            <input onChange={handleEmailChange} value={email} style={{ marginBottom: "32px",width: "calc(100% - 70px)",padding: "16px 20px 16px 50px", left: "15%", alignItems: "center", alignContent: "center", display: "flex", border: "1px solid #d2d8d8"}}>
             </input>
             Parola
-            <input style={{ marginBottom: "32px",width: "calc(100% - 70px)",padding: "16px 20px 16px 50px", left: "15%", alignItems: "center", alignContent: "center", display: "flex", border: "1px solid #d2d8d8"}}>
+            <input onChange={handlePasswordChange} value={password} style={{ marginBottom: "32px",width: "calc(100% - 70px)",padding: "16px 20px 16px 50px", left: "15%", alignItems: "center", alignContent: "center", display: "flex", border: "1px solid #d2d8d8"}}>
 
             </input>
             Parolayı Doğrula
-            <input style={{ marginBottom: "38px", width: "calc(100% - 70px)",padding: "16px 20px 16px 50px", left: "15%", alignItems: "center", alignContent: "center", display: "flex", border: "1px solid #d2d8d8"}}>
+            <input onChange={handlePasswordChange} value={password} style={{ marginBottom: "38px", width: "calc(100% - 70px)",padding: "16px 20px 16px 50px", left: "15%", alignItems: "center", alignContent: "center", display: "flex", border: "1px solid #d2d8d8"}}>
         
             </input>
         </div>
-        <div style={{  width: "70%", borderStyle: "solid", marginLeft: "15%", marginRight: "15%", textAlign: "center", padding: "10px 0px"}}> Kayıt ol</div>
+        <div onClick={handleRegister} style={{  width: "70%", borderStyle: "solid", marginLeft: "15%", marginRight: "15%", textAlign: "center", padding: "10px 0px"}}> Kayıt ol</div>
         
         <div onClick={handleClickLogin} style={{  width: "70%", marginLeft: "15%", marginRight: "15%", textAlign: "center", padding: "10px 0px"}}> Hesabın var mı? Giriş Yap</div>
         </div>
