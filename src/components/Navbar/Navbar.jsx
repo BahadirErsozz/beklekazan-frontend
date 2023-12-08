39c2e954ff03e3c735819e75dfb1927ae1952b0f
BahadirErsozz <68385105+BahadirErsozz@users.noreply.github.com>
2023-12-08 03:09:11 +0300

    change navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,6 +21,9 @@ import { setAddresses } from "../../redux/addresses";
 import Account from "../HoverMenus/Account";
 import SearchBarItems from "../HoverMenus/SearchBarItems";
 
+import location from "./Assets/location.png"
+import user from "./Assets/user.png"
+
 const Navbar = ({}) => {
   const [clickedShoppingCart, setClickedShoppingCart] = useState(false);
   const [clickedAccount, setClickedAccount] = useState(false);
@@ -307,12 +310,13 @@ const Navbar = ({}) => {
           style={{
             display: "block",
             alignItems: "center",
-            minWidth: "20%",
+            minWidth: "auto",
             justifyContent: "center",
             borderRight: "2px solid " + config.BORDER_COLOR,
             flexGrow: "0.6",
           }}
-        >
+        > 
+        
           <div
             onClick={handleClickAddresses}
             style={{
@@ -325,11 +329,14 @@ const Navbar = ({}) => {
               cursor: "pointer",
               justifyContent: "space-between",
             }}
-          >
-            <div style={{ paddingLeft: "20px" }}>
+          > 
+          <div style={{display: "flex", paddingRight: "40px"}}>
+            <img src={location} style={{maxWidth: "28px", maxHeight: "28px", marginLeft: "10px"}}/>
+            <div style={{ paddingLeft: "20px", whiteSpace: "nowrap" }}>
               {selectedAddress?.address_details != undefined
-                ? JSON.parse(selectedAddress?.address_details).address_name
+                ? (<><div>Teslimat Adresi</div><div>{JSON.parse(selectedAddress?.address_details).address_name} </div></>)
                 : "Teslimat Adresi Seçin"}
+            </div>
             </div>
             <div style={{ height: "16px", width: "16px" }}>
               {" "}
@@ -444,27 +451,7 @@ const Navbar = ({}) => {
               justifyContent: "space-between",
             }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <g fill="none" fillRule="evenodd">
-                <circle
-                  cx="12"
-                  cy="12"
-                  r="12"
-                  fill="#D70F64"
-                  fillRule="nonzero"
-                ></circle>
-                <path
-                  fill="#FFF"
-                  fillRule="nonzero"
-                  d="M13.1818252 12.6666667C15.366281 12.6666667 17.1649879 14.3335701 17.3176726 16.4681904L17.3252836 16.6080555 17.3333337 17.0416667C17.3333337 17.1848516 17.2285728 17.3039382 17.0904223 17.3286342L17.0371901 17.3333333 6.96281056 17.3333333C6.81742783 17.3333333 6.69651331 17.2301562 6.67143827 17.0940941L6.666667 17.0416667 6.666667 16.75C6.666667 14.5418198 8.44636147 12.7430258 10.670143 12.6690344L10.8126768 12.6666667 13.1818252 12.6666667zM12.0000003 6C13.6568546 6 15.0000003 7.34314575 15.0000003 9 15.0000003 10.6568543 13.6568546 12 12.0000003 12 10.3431461 12 9.00000033 10.6568543 9.00000033 9 9.00000033 7.34314575 10.3431461 6 12.0000003 6z"
-                ></path>
-              </g>
-            </svg>
+            <img src={user} style={{maxWidth: "28px", maxHeight: "28px", marginLeft: "10px"}}/>
             {isLoggedIn ? (
               <>
                 <div style={{ height: "100%", paddingTop: "20px" }}>
@@ -524,7 +511,7 @@ const Navbar = ({}) => {
               height: "50%",
               alignSelf: "center",
               justifyContent: "flex-end",
-              paddingLeft: "10px",
+              padding: "0 10px",
               cursor: "pointer",
             }}
           >
